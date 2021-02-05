e013d5e2821a1f7d4923c6cfc02f6813eaa0e7a7
njager <nicholasejager@gmail.com>
2021-02-05 18:24:12 -0500

    actually got teleporter to work
    made a janky, dirty solution

--- DELICATE/Assets/Scripts/TeleportScript.cs
+++ DELICATE/Assets/Scripts/TeleportScript.cs
@@ -5,10 +5,35 @@ using UnityEngine;
 public class TeleportScript : MonoBehaviour
 {
     public Transform teleportTarget;
+    public Transform teleportStart;
     public GameObject thePlayer;
+    public GameObject teleporter;
+    private float tpBackTimer = 99999f;
+
+    private void Update()
+    {
+        tpBackTimer -= Time.deltaTime;
+        if (tpBackTimer <= 0f)
+        {
+            TeleportBack();
+        }
+        Debug.Log(tpBackTimer);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
+        TeleportTo();
+    }
+
+    private void TeleportTo()
+    {
         thePlayer.transform.position = teleportTarget.transform.position;
+        tpBackTimer = 3f;
+    }
+
+    private void TeleportBack()
+    {
+        thePlayer.transform.position = teleportStart.transform.position;
+        Destroy(teleporter, 0.1f);
     }
 }
