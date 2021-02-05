9a0f694d2ceb549842846e95687a225581de097e
njager <nicholasejager@gmail.com>
2021-02-05 18:44:46 -0500

    commented up code

---
+++ DELICATE/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerScript : MonoBehaviour
+{
+    //variables
+    private int count;
+    public TextMeshProUGUI countText;
+ 
+    void Start()
+    {
+        SetCountText();
+    }
+
+    //Updates count text with current data, displays win text
+    void SetCountText()
+    {
+        countText.text = "Letters: " + count.ToString();
+    }
+
+    //called when player enters a trigger
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //if the trigger is a pick up, update count and play sound
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+
+            SetCountText();
+        }
+
+    }
+}
--- DELICATE/Assets/Scripts/TeleportScript.cs
+++ DELICATE/Assets/Scripts/TeleportScript.cs
@@ -4,33 +4,40 @@ using UnityEngine;
 
 public class TeleportScript : MonoBehaviour
 {
+    //public variable to implement via inspector
     public Transform teleportTarget;
     public Transform teleportStart;
     public GameObject thePlayer;
     public GameObject teleporter;
-    private float tpBackTimer = 99999f;
 
+    //private variables 
+    private float tpBackTimer = 99999f; //is super long so that it won't get ticked down just by playing
+
+    //called every frame
     private void Update()
     {
         tpBackTimer -= Time.deltaTime;
+        //after the timer is set to its real timer, the above time decrease will cause it to hit 0 then teleport player back
         if (tpBackTimer <= 0f)
         {
             TeleportBack();
         }
-        Debug.Log(tpBackTimer);
     }
 
+    //called when player enters the teleport pad
     private void OnTriggerEnter(Collider other)
     {
         TeleportTo();
     }
 
+    //teleports the player to the transform position of the teleport target then sets back timer
     private void TeleportTo()
     {
         thePlayer.transform.position = teleportTarget.transform.position;
         tpBackTimer = 3f;
     }
 
+    //teleports player back to starting location and deletes the whole teleporter to avoid infinite teleporting freezing player
     private void TeleportBack()
     {
         thePlayer.transform.position = teleportStart.transform.position;
