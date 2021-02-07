619b2402ae54b0bf62dc7d9b4530955337252792
aemeadow <aemeadow@iu.edu>
2021-02-07 18:12:02 -0500

    text fixed
    text fixed

---
+++ DELICATE/Assets/Scripts/EndScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider col)
+    {
+
+        Debug.Log("End");
+
+        if (col.gameObject.tag == "Player")
+        {
+
+            float endTimer = 5f;
+            endTimer -= Time.deltaTime;
+            if(endTimer <= 0f )
+            {
+                Application.Quit();
+            }
+
+
+        }
+    }
+}
